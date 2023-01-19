056a307844b35e1f5f7b1c7f9eea8cda2d85ee81
Ash-004 <ash_004@icloud.com>
2023-01-19 21:06:36 +0530

    Final touches

--- mysite/settings.py
+++ mysite/settings.py
@@ -100,7 +100,21 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR / 'templates'],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
